2127cd63fbab58dac73ffaeff8697135243e2e4f
João Vitor Damaceno <106777174+joaovdamaceno@users.noreply.github.com>
2026-02-24 08:26:51 -0300

    Fix GET exercises lazy loading error

--- src/main/java/br/unioeste/mu/mu_backend/exercise/ExerciseRepository.java
+++ src/main/java/br/unioeste/mu/mu_backend/exercise/ExerciseRepository.java
@@ -1,12 +1,14 @@
 package br.unioeste.mu.mu_backend.exercise;
 
 import br.unioeste.mu.mu_backend.module.Module;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface ExerciseRepository extends JpaRepository<Exercise, Long> {
 
+    @EntityGraph(attributePaths = "tags")
     List<Exercise> findByModule(Module module);
 
     List<Exercise> findByModuleIdInOrderByModuleIdAscIdAsc(List<Long> moduleIds);
