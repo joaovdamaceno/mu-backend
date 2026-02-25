614690771ce51084e0b4c693a372d12ef16d0dca
João Vitor Damaceno <106777174+joaovdamaceno@users.noreply.github.com>
2026-02-24 23:19:06 -0300

    Remove duplicated V16 migration

--- src/main/resources/db/migration/V16__simplify_module_content_fields.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE lessons
-    DROP COLUMN IF EXISTS slug,
-    DROP COLUMN IF EXISTS summary;
-
-ALTER TABLE exercises
-    DROP COLUMN IF EXISTS oj_name;
-
-ALTER TABLE extra_materials
-    RENAME COLUMN type TO title;
